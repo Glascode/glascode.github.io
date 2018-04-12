eb3c584cb83c1c1c2ad71880e81b5632bc6628ac
Glascode <mael.querre@gmail.com>
2018-04-12 17:04:56 +0200

    Update 404 page

--- scripts/404.js
+++ scripts/404.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    var statusButtons = $(".status-buttons");
+    statusButtons.mouseover(function () {
+        this.classList.add('active');
+    });
+
+    statusButtons.mouseout(function () {
+        this.classList.remove('active');
+    });
+
     var helpIcon = $(".help-icon");
 
     helpIcon.mouseover(function () {
